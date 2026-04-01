6795b055eb0166e13f6149bfe562f49c5ff53dfa
ameeshajaswal <91967935+ameeshajaswal@users.noreply.github.com>
2026-04-01 14:32:35 -0400

    Remove basename for Router in App component
    Removed basename configuration for Router in App component.

--- my-app/Client/src/App.jsx
+++ my-app/Client/src/App.jsx
@@ -13,15 +13,10 @@ import SignUp from './pages/SignUp';
 import UserProfile from './pages/UserProfile';
 import Education from './pages/Education';
 
-// Add basename for GitHub Pages deployment
-// When running locally, basename will be '/'
-// When deployed to GitHub Pages, basename will be '/Portfolio'
-const basename = process.env.NODE_ENV === 'production' ? '/Portfolio' : '/';
-
 function App() {
   return (
     <AuthProvider>
-      <Router basename={basename}>
+      <Router>
         <div className="flex flex-col min-h-screen">
           <Navbar />
           <main className="flex-grow">
